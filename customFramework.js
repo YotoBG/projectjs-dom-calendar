1dd7b61647b540437876542edc6ffb886527edd1
YotoBG <yoto721@abv.bg>
2019-02-14 16:38:52 +0200

    saveWithOldCustomLib

--- customFramework.js
+++ customFramework.js
@@ -1,30 +1,36 @@
 //domC custom mini framework
 var domC = { 
 
-    //1. Gets existing element (by id) method
+    
+    // 1. Gets existing element (by id) method
     get: function(id) {
         return document.getElementById(id);
     
     },
 
-    //3. Deletes element by id method
+    // 3. Deletes element by id method
     delete: function(id){
         this.get(id).remove();
     },
 
-    //Adding text to element by id 
-    text: function(id, text) {
-        var innerElement = this.get(id);
-        var textNode = document.createTextNode(text);
-        innerElement.appendChild(textNode);
+    // 4c. Adding text to element by id 
+     text: function(id, text) {
+         var innerElement = this.get(id);
+         var textNode = document.createTextNode(text);
+         innerElement.appendChild(textNode);
+     },
+
+    textIn: function(existingEl, text) {
+        var textNode1 = document.createTextNode(text);
+        existingEl.appendChild(textNode1);
     },
 
-    //2. Adding a non-existing element to an existing one 
-    addIn(newElType,existingElementID,newElInnerHTML){
+    // 2. Adding a non-existing element to an existing one 
+    addIn: function(newElType,existingElementID,newElInnerHTML){
         
         var newElement = document.createElement(newElType);
         
-        //If value is entered for inner element text, add it in the newElement
+        // If value is entered for inner element text, add it in the newElement
         if(newElInnerHTML !== undefined){
             var node = document.createTextNode(newElInnerHTML);
             newElement.appendChild(node);
@@ -35,6 +41,88 @@ var domC = {
         //return existingElement;
     },
 
+    // 4. Altering html element
+    // a. Atributes 
+    // Change ID by ID
+    chageAttributeId: function(AlterAtrbId,newId){
+        let alterElement = this.get(AlterAtrbId);
+        
+        if (validStringCheck(newId)){
+           alterElement.id=newId;  
+        }
+        
+    },
+
+    // Change Class by ID
+    chageAttributeClass: function(AlterAtrbId,newClass){
+        let alterElement = this.get(AlterAtrbId);
+        
+        if (validStringCheck(newClass)){
+           alterElement.className = newClass;
+        }
+        
+    },
+
+    // Changes attribute name by ID
+    chageAttributeName: function(AlterAtrbId,newName){
+            let alterElement = this.get(AlterAtrbId);
+            
+            if (validStringCheck(newName)){
+               alterElement.setAttribute("name",newName);
+            }
+            
+        },
+    
+    //TODO : data attribute
+
+    // 4d. Changing an element's existing style/s by it's ID
+    // Adding styles from an object
+    changeStyle: function(elemId,objectStyles){
+        var alterElementSt = this.get(elemId);
+        
+
+        if (typeof(objectStyles)==='object'){
+            var styleValue = "";
+            for(i in objectStyles){
+                styleValue += i + ": " + objectStyles[i] +";";
+                //console.log("ST VALUE: "+styleValue);    
+                alterElementSt.setAttribute("style",styleValue);
+            }
+
+        }
+        
+        //return alterElementSt;
+    },
+
+    // 4d. Changing element's existing styles by passing the element itself as a argument
+    changeStyleExistingEl: function(elementExisting,objectStyles){
+        
+        if ((typeof(objectStyles)==='object') && (elementExisting instanceof HTMLElement)){
+            var styleValue = "";
+            for(i in objectStyles){
+                styleValue += i + ": " + objectStyles[i] +";";   
+                elementExisting.setAttribute("style",styleValue);
+            }
+        }
+        return this;
+    },
+
+    // 4b. ?
+
+    // 5d gets child el array
+    getChildElement: function(parElId){
+        var parEl = document.getElementById(parElId);
+        var chNodes = parEl.childNodes;
+        return chNodes;
+    }
+
 
 };
 
+domC.proto
+
+var validStringCheck = function(myString){
+if((myString !== "") && (myString !== undefined)){
+return true;
+}
+}
--- scripts.js
+++ scripts.js
@@ -4,3 +4,86 @@ domC.delete("test");
 domC.text("test1","hello there ")
 
 domC.addIn("h1","testp");
+
+domC.chageAttributeId("testp","id-set-test");
+domC.chageAttributeClass("test1","class-set-test");
+domC.chageAttributeName("test1","name-set-test");
+
+var dateO = new Date();
+var mountNumber = dateO.getMonth() + 1;
+var dayNumber = dateO.getDate();
+console.log(dayNumber);
+var mountText = "";
+
+switch(mountNumber) {
+    case 1:
+    mountText = "January";
+      break;
+    case 2:
+    mountText = "February";
+      break;
+    case 3:
+    mountText = "March";
+      break;
+    case 4:
+    mountText = "Aril";
+      break;
+    case 5:
+    mountText = "May";
+      break;
+    case 6:
+    mountText = "June";
+      break;
+    case 7:
+    mountText = "July";
+      break;
+    case 8:
+    mountText = "August";
+      break;
+    case 9:
+    mountText = "September";
+      break;
+    case 10:
+    mountText = "October";
+      break;
+    case 1:
+    mountText = "November";
+        break;
+    case 2:
+    mountText = "December";
+        break;
+    default:
+    mountText = "Error";
+  }
+
+domC.get("mount-calendar").innerHTML = mountText;
+console.log("==== Key Value Test ====");
+
+var object2 = {
+    color: "blue",
+    "background-color": "red",
+    "font-weight": "normal",
+}
+
+{
+// for(var i in object2){
+//     console.log("OBJECT2 Key:" + i + ". Value: " + object2[i]); 
+//                //  key                     // key's value
+// }
+}
+domC.changeStyle("key-value-test",object2);
+
+//===============================================
+console.log("==== Get Child Element Test ====");
+
+var childElementArray = domC.getChildElement("table-tr");
+
+for (let i = 1; i < childElementArray.length; i+=2) {
+
+    domC.changeStyleExistingEl(childElementArray[i],object2);
+    domC.textIn(childElementArray[i],dayNumber);
+    
+    //console.log(childElementArray[i]);
+}
+
+//console.log(domC.getChildElement("table-tr"))
